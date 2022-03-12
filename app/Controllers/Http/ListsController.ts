e7eb81a93c2858f9b5d24404891cf354453f6934
Militao36 <mathuesmoreira3693@gmail.com>
2022-03-12 13:40:40 -0300

    Commit

--- app/Controllers/Http/ListsController.ts
+++ app/Controllers/Http/ListsController.ts
@@ -2,13 +2,19 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import List from 'App/Models/List'
 
 export default class ListsController {
-  public async index({ }: HttpContextContract) {
-
-  }
+  public async index({ }: HttpContextContract) { }
 
   public async show({ }: HttpContextContract) { }
 
-  public async create({ }: HttpContextContract) { }
+  public async create({ request, response }: HttpContextContract) {
+    const data = request.only(['title'])
+
+    const { id } = await List.create({
+      title: data.title
+    })
+
+    return response.created({ id })
+  }
 
   public async store({ }: HttpContextContract) { }
 
--- start/routes/lists.ts
+++ start/routes/lists.ts
@@ -2,4 +2,5 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.get('/', 'ListsController.index')
+  Route.post('/', 'ListsController.create')
 }).prefix('/lists')
